cb2e97e612de80cb491e333e1c3e5e4a746dc712
Hank Kim <hank29206880@gmail.com>
2023-04-07 13:31:34 +0900

    regex추가

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,12 +1,44 @@
-import { Alert, Pressable, StyleSheet, Text, View } from 'react-native';
+import {
+  Alert,
+  Keyboard,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Ionicons } from '@expo/vector-icons';
 import Input from '../components/common/Input';
 import Button from '../components/common/Button';
 import { useNavigation } from '@react-navigation/native';
+import { useState } from 'react';
+import { StackParamList } from '../../AppInner';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { validate } from '../utils/validate';
+
+export interface SignUpProps {
+  email: string;
+  password: string;
+  confirmPassowrd: string;
+}
 
 const SignUpPage = () => {
-  const navigation = useNavigation();
+  const navigation =
+    useNavigation<NativeStackNavigationProp<StackParamList, 'SignUp'>>();
+  const [input, setInput] = useState<SignUpProps>({
+    email: '',
+    password: '',
+    confirmPassowrd: '',
+  });
+  const [error, setError] = useState('');
+  const onSignUp = () => {
+    if (validate(input) === '') {
+      Alert.alert('회원가입이 완료되었습니다.');
+      navigation.navigate('SignIn');
+    } else {
+      setError(validate(input));
+    }
+  };
 
   return (
     <SafeAreaView style={{ alignItems: 'center' }}>
@@ -21,22 +53,25 @@ const SignUpPage = () => {
           <Text style={styles.title}>SIGN UP</Text>
         </View>
       </View>
-      <Input style={{ marginTop: '25%' }} placeholder="이메일" />
+      <Input
+        style={{ marginTop: '25%' }}
+        placeholder="이메일"
+        onChangeText={text => setInput({ ...input, email: text })}
+      />
       <Input
         style={{ marginTop: 12 }}
         placeholder="비밀번호 (8~16자리)"
         secureTextEntry={true}
+        onChangeText={text => setInput({ ...input, password: text })}
       />
       <Input
         style={{ marginTop: 12 }}
         placeholder="비밀번호 확인"
         secureTextEntry={true}
+        onChangeText={text => setInput({ ...input, confirmPassowrd: text })}
       />
-      <Button
-        text="회원가입"
-        style={{ marginTop: 34 }}
-        onPress={() => Alert.alert('회원가입이 완료되었습니다.')}
-      />
+      {error !== '' && <Text style={styles.err}>{error}</Text>}
+      <Button text="회원가입" style={{ marginTop: 34 }} onPress={onSignUp} />
     </SafeAreaView>
   );
 };
@@ -45,6 +80,13 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     fontWeight: '700',
+  },
+  err: {
+    textAlign: 'left',
+    marginRight: 'auto',
+    marginLeft: 65,
+    marginTop: 10,
+    color: 'red',
   },
 });
 
